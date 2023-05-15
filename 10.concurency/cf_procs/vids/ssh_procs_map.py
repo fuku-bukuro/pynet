dca6bb328d41d5a2ba4f933e04cec9888040e23d
Tudor <listinguri@gmail.com>
2023-05-15 10:58:35 -0700

    class 10

---
+++ 10.concurency/cf_procs/vids/ssh_procs_map.py
@@ -0,0 +1,30 @@
+import time
+from concurrent.futures import ProcessPoolExecutor 
+from datetime import datetime
+from netmiko import ConnectHandler
+from my_devices import device_list
+
+
+def ssh_conn(device):
+    net_connect = ConnectHandler(**device)
+    return net_connect.find_prompt()
+
+
+
+if __name__ == "__main__":
+
+    start_time = datetime.now()
+    max_threads = 4
+
+    # use context manager to gracefully cleanup the pool
+    with ProcessPoolExecutor(max_threads) as pool:
+        results_generator = pool.map(ssh_conn, device_list)
+
+        # Results generator 
+        for result in results_generator:
+            print(result)
+        
+        end_time = datetime.now()
+        print(end_time - start_time)
+
+
---
+++ 10.concurency/cf_threads/vids/ssh_threads_map2.py
@@ -0,0 +1,33 @@
+import time
+from concurrent.futures import ThreadPoolExecutor 
+from datetime import datetime
+from netmiko import ConnectHandler
+from my_devices import device_list
+
+
+def ssh_conn(device):
+    return_dict ={}
+    net_connect = ConnectHandler(**device)
+    dns_name = net_connect.host
+    my_prompt = net_connect.find_prompt()
+    return_dict[dns_name] = my_prompt
+    return return_dict
+
+
+
+if __name__ == "__main__":
+
+    start_time = datetime.now()
+    max_threads = 4
+
+    # use context manager to gracefully cleanup the pool
+    with ThreadPoolExecutor(max_threads) as pool:
+        results_generator = pool.map(ssh_conn, device_list)
+
+        # Results generator 
+        for result in results_generator:
+            print(result)
+            end_time = datetime.now()
+            print(end_time - start_time)
+
+
